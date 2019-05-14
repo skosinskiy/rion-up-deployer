c3c3ecff7d80aa69b0d94277e50622e33ee598c3
Stanislav Kosinskiy <stanislav.kosinski@gmail.com>
2019-05-14 21:03:01 +0300

    refactored init endpoint end added timeout for curl

--- src/main/java/com/danit/rionup/starter/rionupstarter/controller/DeployController.java
+++ src/main/java/com/danit/rionup/starter/rionupstarter/controller/DeployController.java
@@ -32,24 +32,7 @@ public class DeployController {
 
   @GetMapping("deploy/init")
   public ResponseEntity<String> initTravisBuild(@RequestParam String branch) throws ParseException {
-    String url = "https://api.travis-ci.com/repo/8174216/requests";
-    String json = "{\n" +
-        "    \"request\": {\n" +
-        "        \"branch\": \"" + branch + "\",\n" +
-        "        \"config\": {\n" +
-        "            \"after_success\": \"curl -X POST http://ec2-3-14-226-139.us-east-2.compute.amazonaws.com:9002/deploy -F 'jar=@/home/travis/build/skosinskiy/dan-it-final-project/target/final-project-0.0.1-SNAPSHOT.jar' -F 'branch="+ branch + "'\"\n" +
-        "        }\n" +
-        "    }\n" +
-        "}";
-    System.out.println(json);
-
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_JSON);
-    headers.set("Authorization", "token YNwrtvmNH9NU015vVAGHIQ");
-    headers.set("Travis-API-Version", "3");
-
-    HttpEntity<Object> requestEntity = new HttpEntity<>(new JSONParser(json).parse(), headers);
-    return new RestTemplate().postForEntity(url, requestEntity, String.class);
+    return deployService.initTravisBuildWithDeploy(branch);
   }
 
 }
--- src/main/java/com/danit/rionup/starter/rionupstarter/service/DeployService.java
+++ src/main/java/com/danit/rionup/starter/rionupstarter/service/DeployService.java
@@ -1,6 +1,13 @@
 package com.danit.rionup.starter.rionupstarter.service;
 
+import org.apache.tomcat.util.json.JSONParser;
+import org.apache.tomcat.util.json.ParseException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -16,6 +23,7 @@ public class DeployService {
   private static final String PROD_JAR_PATH = ARTIFACTS_PATH + PROD_JAR_NAME;
   private static final String DEV_JAR_NAME = "rion-up-application-dev.jar";
   private static final String DEV_JAR_PATH = ARTIFACTS_PATH + DEV_JAR_NAME;
+  private static final String TRAVIS_REPOSITORY_BUILD = "https://api.travis-ci.com/repo/8174216/requests";
 
   public String deployJar(MultipartFile jar, String branch) throws IOException, InterruptedException {
     Map<String, String> commands = getCommandsByBranch(branch);
@@ -42,12 +50,29 @@ public class DeployService {
   }
 
   private void executeCommand(String command) throws IOException, InterruptedException {
-    System.out.println(System.currentTimeMillis() + command);
     ProcessBuilder builder = new ProcessBuilder(command.split(" "));
     builder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
     builder.redirectError(ProcessBuilder.Redirect.INHERIT);
     Process process = builder.start();
     process.waitFor();
-    System.out.println(System.currentTimeMillis() + process.exitValue());
+  }
+
+  public ResponseEntity<String> initTravisBuildWithDeploy(String branch) throws ParseException {
+    String json = "{\n" +
+        "    \"request\": {\n" +
+        "        \"branch\": \"" + branch + "\",\n" +
+        "        \"config\": {\n" +
+        "            \"after_success\": \"curl -X POST http://ec2-3-14-226-139.us-east-2.compute.amazonaws.com:9002/deploy -F 'jar=@/home/travis/build/skosinskiy/dan-it-final-project/target/final-project-0.0.1-SNAPSHOT.jar' -F 'branch="+ branch + "' --max-time 15\"\n" +
+        "        }\n" +
+        "    }\n" +
+        "}";
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    headers.set("Authorization", "token YNwrtvmNH9NU015vVAGHIQ");
+    headers.set("Travis-API-Version", "3");
+
+    HttpEntity<Object> requestEntity = new HttpEntity<>(new JSONParser(json).parse(), headers);
+    return new RestTemplate().postForEntity(TRAVIS_REPOSITORY_BUILD, requestEntity, String.class);
   }
 }
