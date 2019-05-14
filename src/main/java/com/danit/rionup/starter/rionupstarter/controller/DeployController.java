9d452be3d87892aa70088a2644435dfef6218128
Stanislav Kosinskiy <stanislav.kosinski@gmail.com>
2019-05-15 02:19:13 +0300

    added slack bot integration

--- src/main/java/com/danit/rionup/starter/rionupstarter/controller/DeployController.java
+++ src/main/java/com/danit/rionup/starter/rionupstarter/controller/DeployController.java
@@ -1,19 +1,14 @@
 package com.danit.rionup.starter.rionupstarter.controller;
 
 import com.danit.rionup.starter.rionupstarter.service.DeployService;
-import org.apache.tomcat.util.json.JSONParser;
-import org.apache.tomcat.util.json.ParseException;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
 
 @RestController
@@ -30,9 +25,11 @@ public class DeployController {
     return deployService.deployJar(jar, branch);
   }
 
-  @GetMapping("deploy/init")
-  public ResponseEntity<String> initTravisBuild(@RequestParam String branch) throws ParseException {
-    return deployService.initTravisBuildWithDeploy(branch);
+  @PostMapping("deploy/init")
+  public ResponseEntity<Object> initTravisBuildFromSlack(@RequestBody(required = false) String body) {
+    deployService.initTravisBuildFromSlack(body);
+    return new ResponseEntity<>(HttpStatus.OK);
+
   }
 
 }
--- src/main/java/com/danit/rionup/starter/rionupstarter/service/DeployService.java
+++ src/main/java/com/danit/rionup/starter/rionupstarter/service/DeployService.java
@@ -1,19 +1,28 @@
 package com.danit.rionup.starter.rionupstarter.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.tomcat.util.json.JSONParser;
 import org.apache.tomcat.util.json.ParseException;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class DeployService {
@@ -23,9 +32,21 @@ public class DeployService {
   private static final String PROD_JAR_PATH = ARTIFACTS_PATH + PROD_JAR_NAME;
   private static final String DEV_JAR_NAME = "rion-up-application-dev.jar";
   private static final String DEV_JAR_PATH = ARTIFACTS_PATH + DEV_JAR_NAME;
-  private static final String TRAVIS_REPOSITORY_BUILD = "https://api.travis-ci.com/repo/8174216/requests";
+
+  private static final String SLACK_MESSAGE_URL =
+      "https://hooks.slack.com/services/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";
+
+  private static final String TRAVIS_TOKEN = "token YNwrtvmNH9NU015vVAGHIQ";
+  private static final String TRAVIS_API_VERSION = "3";
+  private static final String TRAVIS_BASE_URL = "https://api.travis-ci.com/repo";
+  private static final String TRAVIS_REPOSITORY_ID = "8174216";
+  private static final String TRAVIS_REPOSITORY_BUILD_URL =
+      String.format("%s/%s/requests", TRAVIS_BASE_URL, TRAVIS_REPOSITORY_ID);
+  private static final String TRAVIS_REPOSITORY_BRANCHES_URL =
+      String.format("%s/%s/branches?exists_on_github=true", TRAVIS_BASE_URL, TRAVIS_REPOSITORY_ID);
 
   public String deployJar(MultipartFile jar, String branch) throws IOException, InterruptedException {
+    sendMessageToSlack("Build artifact successfully sent to AWS");
     Map<String, String> commands = getCommandsByBranch(branch);
     File appFile = new File(commands.get("filePath"));
     if (appFile.exists()) {
@@ -33,8 +54,13 @@ public class DeployService {
     }
     jar.transferTo(new File(commands.get("filePath")));
 
-    executeCommand(commands.get("runScript"));
+    executeChecks();
+    executeShellCommand(commands.get("runScript"));
     return "successfully";
+  }
+
+  private void executeChecks() {
+    //TODO implement this method
   }
 
   private Map<String, String> getCommandsByBranch(String branch) {
@@ -49,7 +75,7 @@ public class DeployService {
     return result;
   }
 
-  private void executeCommand(String command) throws IOException, InterruptedException {
+  private void executeShellCommand(String command) throws IOException, InterruptedException {
     ProcessBuilder builder = new ProcessBuilder(command.split(" "));
     builder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
     builder.redirectError(ProcessBuilder.Redirect.INHERIT);
@@ -57,22 +83,124 @@ public class DeployService {
     process.waitFor();
   }
 
-  public ResponseEntity<String> initTravisBuildWithDeploy(String branch) throws ParseException {
+  public List<String> getActiveTravisBranches() throws IOException {
+    ResponseEntity<String> response =
+        sendRequest(TRAVIS_REPOSITORY_BRANCHES_URL, HttpMethod.GET, getTravisHeaders());
+    ObjectNode rootNode = new ObjectMapper().readValue(response.getBody(), ObjectNode.class);
+    if (rootNode.has("branches")) {
+      JsonNode branchesNode = rootNode.get("branches");
+      return branchesNode.findValues("name")
+          .stream()
+          .map(JsonNode::asText)
+          .collect(Collectors.toList())
+          ;
+    }
+    return new ArrayList<>();
+  }
+
+  public String initTravisBuildWithDeploy(String branch) throws ParseException, IOException {
     String json = "{\n" +
         "    \"request\": {\n" +
         "        \"branch\": \"" + branch + "\",\n" +
         "        \"config\": {\n" +
-        "            \"after_success\": \"curl -X POST http://ec2-3-14-226-139.us-east-2.compute.amazonaws.com:9002/deploy -F 'jar=@/home/travis/build/skosinskiy/dan-it-final-project/target/final-project-0.0.1-SNAPSHOT.jar' -F 'branch="+ branch + "' --max-time 15\"\n" +
+        "            \"after_success\": \"curl -X POST http://ec2-3-14-226-139.us-east-2.compute.amazonaws.com:9002/deploy -F 'jar=@/home/travis/build/skosinskiy/dan-it-final-project/target/final-project-0.0.1-SNAPSHOT.jar' -F 'branch=" + branch + "' --max-time 15\"\n" +
         "        }\n" +
         "    }\n" +
         "}";
+    ResponseEntity<String> response =
+        sendRequest(TRAVIS_REPOSITORY_BUILD_URL, json, HttpMethod.POST, getTravisHeaders());
+    ObjectNode rootNode = new ObjectMapper().readValue(response.getBody(), ObjectNode.class);
+    if (rootNode.has("request")) {
+      JsonNode requestNode = rootNode.get("request");
+      if (requestNode.has("id")) {
+        return requestNode.get("id").asText();
+      }
+    }
+    return "";
+  }
 
+  public void sendMessageToSlack(String message) {
+    try {
+      String body = String.format("{\"text\":\"%s\"}", message);
+      sendRequest(SLACK_MESSAGE_URL, body, HttpMethod.POST, getSlackHeaders());
+    } catch (ParseException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private HttpHeaders getSlackHeaders() {
     HttpHeaders headers = new HttpHeaders();
     headers.setContentType(MediaType.APPLICATION_JSON);
-    headers.set("Authorization", "token YNwrtvmNH9NU015vVAGHIQ");
-    headers.set("Travis-API-Version", "3");
+    return headers;
+  }
 
-    HttpEntity<Object> requestEntity = new HttpEntity<>(new JSONParser(json).parse(), headers);
-    return new RestTemplate().postForEntity(TRAVIS_REPOSITORY_BUILD, requestEntity, String.class);
+  private HttpHeaders getTravisHeaders() {
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    headers.set("Authorization", TRAVIS_TOKEN);
+    headers.set("Travis-API-Version", TRAVIS_API_VERSION);
+    return headers;
+  }
+
+  private ResponseEntity<String> sendRequest(
+      String url,
+      String body,
+      HttpMethod method,
+      HttpHeaders headers) throws ParseException {
+
+    HttpEntity<Object> requestEntity = new HttpEntity<>(new JSONParser(body).parse(), headers);
+    return new RestTemplate().exchange(url, method, requestEntity, String.class);
+  }
+
+  private ResponseEntity<String> sendRequest(String url, HttpMethod method, HttpHeaders headers) {
+    HttpEntity<Object> requestEntity = new HttpEntity<>(headers);
+    return new RestTemplate().exchange(url, method, requestEntity, String.class);
+  }
+
+  public String getBranchFromSlackEvent(String body) throws IOException {
+    ObjectNode rootNode = new ObjectMapper().readValue(body, ObjectNode.class);
+    if (rootNode.has("event")) {
+      JsonNode event = rootNode.get("event");
+      if (event.has("type") && event.has("text")) {
+        if ("message".equals(event.get("type").asText())) {
+          String text = event.get("text").asText();
+          String command = Arrays.stream(text.split(" ")).findFirst().orElse("");
+          if ("deploy".equals(command)) {
+            String branchString = Arrays.stream(text.split(" "))
+                .filter(string -> string.contains("branch:"))
+                .findFirst()
+                .orElse("");
+            return branchString.split(":")[1];
+          }
+        }
+      }
+    }
+    return "";
+  }
+
+  public boolean isBranchActive(String branchName) throws IOException {
+    List<String> activeTravisBranches = getActiveTravisBranches();
+    return activeTravisBranches.contains(branchName);
+  }
+
+  public void initTravisBuildFromSlack(String body) {
+    try {
+      String branchName = getBranchFromSlackEvent(body);
+      if (StringUtils.hasText(branchName)) {
+        if (isBranchActive(branchName)) {
+          String requestId = initTravisBuildWithDeploy(branchName);
+          if (StringUtils.hasText(requestId)) {
+            sendMessageToSlack(
+                String.format("Travis build started: branch=%s requestId=%s", branchName, requestId));
+          }
+        } else {
+          sendMessageToSlack(
+              String.format("Branch %s not found in active repository branches!", branchName));
+        }
+      }
+
+    } catch (Exception e) {
+      sendMessageToSlack("Failed to initialize build: " + e.getMessage());
+    }
   }
 }
